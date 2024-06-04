5d104d375acf09e2c130545eb7fb84cb7570af6d
imran <87297004+imranshaikh9930@users.noreply.github.com>
2024-06-05 00:10:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+const hamburger = document.querySelector(".hamburger");
+const listItem = document.querySelector(".list__item");
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  listItem.classList.toggle("active");
+});
